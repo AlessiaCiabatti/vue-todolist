273e820485845842a7fc2bb30814c4c5c2380b19
AlessiaCiabatti <alessiaciaba@gmail.com>
2024-03-13 16:23:13 +0100

    [add] js-addNewTask

--- js/script.js
+++ js/script.js
@@ -20,7 +20,11 @@ createApp({
           // 2
           done: true,
         },
-      ]
+      ],
+
+      // 4
+      newTask: '',
+
     }
   },
 
@@ -30,6 +34,20 @@ createApp({
       console.log(indice);
       // 3 splice, 1: elimina solo un solo elemento e restituisce l'array con gli elementi rimanenti
       this.todoList.splice(indice, 1);
+    },
+
+    // 4
+    addNewTask(){
+      console.log('aggiungi');
+      //  unshift: aggiunge all'inizio e non alla fine
+      // tra le graffe perchè aggiungi un oggetto e non una stringa
+      this.todoList.unshift({
+        thingsToDo: this.newTask, 
+        done: false
+    });
+
+      // dopo l'invio rimane vuoto
+      this.newTask = '';
     }
   }
 }).mount('#app');
